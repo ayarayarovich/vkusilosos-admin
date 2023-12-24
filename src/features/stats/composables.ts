a556267e373d2ff03958018e92f06e1468c8b767
Ayyar Khalilov <super.ayar-04@yandex.ru>
2023-12-24 15:15:04 +0300

    add stats

---
+++ src/features/stats/composables.ts
@@ -0,0 +1,67 @@
+import { computed, unref, type MaybeRef } from 'vue'
+import { useQuery, useQueryClient } from '@tanstack/vue-query'
+import { axiosPrivate } from '@/network'
+import { pad } from '@/utils'
+
+interface GetStatsResponse {
+  clients_count: number
+  last_price: number
+  new_clients: number
+  new_reviews: number
+  orders: number
+  price: number
+  stats: { date: string; price: number; count: number; new_users: number }[]
+}
+
+interface QueryConfig {
+  from: MaybeRef<Date>
+  to: MaybeRef<Date>
+}
+
+export const useStats = <SData>(
+  queryConfig: QueryConfig,
+  selector?: (response: GetStatsResponse) => SData
+) => {
+  const { from, to } = queryConfig
+
+  console.log(from, to)
+
+  const _from = computed(
+    () =>
+      unref(from).getFullYear() +
+      '-' +
+      pad(unref(from).getMonth() + 1, 2) +
+      '-' +
+      pad(unref(from).getDate(), 2)
+  )
+  const _to = computed(
+    () =>
+      unref(to).getFullYear() +
+      '-' +
+      pad(unref(to).getMonth() + 1, 2) +
+      '-' +
+      pad(unref(to).getDate(), 2)
+  )
+
+  return useQuery({
+    queryKey: ['stats', { from: _from, to: _to }] as any,
+    queryFn: async ({ queryKey }) => {
+      const response = await axiosPrivate.post<GetStatsResponse>('admin/stats', {
+        from: (queryKey[1] as any).from,
+        to: (queryKey[1] as any).to
+      })
+
+      return response.data
+    },
+    select: selector,
+    keepPreviousData: true
+  })
+}
+
+export const useInvalidateStats = () => {
+  const queryClient = useQueryClient()
+
+  return () => {
+    queryClient.invalidateQueries(['stats'])
+  }
+}
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,28 +1,66 @@
 <script setup lang="ts">
-function getGradient(ctx: any, chartArea: any) {
-  let gradient = ctx.createLinearGradient(0, chartArea.bottom, 0, chartArea.top)
-  gradient.addColorStop(1, 'rgba(0, 147, 233, 0.5)')
-  gradient.addColorStop(0, 'rgba(128, 208, 199, 0.2)')
-  return gradient
+import { useStats, useInvalidateStats } from '@/features/stats'
+import { computed, ref } from 'vue'
+
+// const dates = ref<Date[]>([])
+const defaultFrom = new Date(Date.now() - 2629746000)
+const defaultTo = new Date(Date.now())
+const dates = ref<Date[]>([defaultFrom, defaultTo])
+
+const from = computed(() => (dates.value[0] ? dates.value[0] : defaultFrom))
+const to = computed(() => (dates.value[1] ? dates.value[1] : defaultTo))
+
+const { data: stats } = useStats(
+  {
+    from,
+    to
+  },
+  (v) => v
+)
+
+const invalidateStats = useInvalidateStats()
+
+const refreshStats = () => {
+  invalidateStats()
 }
+
+const labels = computed(() => stats.value?.stats.map((v) => v.date) || [])
+const income = computed(() => stats.value?.stats.map((v) => v.price) || [])
+const ordersCount = computed(() => stats.value?.stats.map((v) => v.count) || [])
+const newUsersCount = computed(() => stats.value?.stats.map((v) => v.new_users) || [])
 </script>
 
 <template>
   <main class="flex flex-col items-stretch px-4">
-    <h1 class="my-12 text-center text-3xl font-semibold leading-none text-black">Дашборд</h1>
-    <div class="mb-8 flex items-stretch gap-4">
+    <h1 class="mb-4 mt-12 text-center text-3xl font-semibold leading-none text-black">Дашборд</h1>
+
+    <div class="mb-4 flex items-center justify-between gap-4">
+      <div class="flex flex-1 justify-center">
+        <Calendar
+          showButtonBar
+          class="w-full"
+          v-model="dates"
+          selectionMode="range"
+          :manualInput="false"
+        />
+      </div>
+      <div class="flex-1"></div>
+
+      <div class="flex-1"></div>
+      <div class="flex flex-1 justify-end">
+        <Button severity="primary" icon="pi pi-refresh" @click="refreshStats" />
+      </div>
+    </div>
+    <div class="mb-16 flex items-stretch gap-4">
       <div class="flex-1 rounded-lg border bg-white p-8">
         <div class="mb-4 flex items-start justify-between text-black">
           <div>
             <div class="mb-2 font-medium">Заказы</div>
-            <div class="text-lg font-bold">152</div>
+            <div class="text-lg font-bold">{{ stats?.orders }}</div>
           </div>
           <div class="flex aspect-square h-10 items-center justify-center rounded-lg bg-orange-100">
             <i class="pi pi-fw pi-shopping-cart" />
           </div>
-        </div>
-        <div class="text-sm">
-          <span class="font-medium text-green-500">24 новых заказа</span> с последнего посещения
         </div>
       </div>
 
@@ -30,14 +68,15 @@ function getGradient(ctx: any, chartArea: any) {
         <div class="mb-4 flex items-start justify-between text-black">
           <div>
             <div class="mb-2 font-medium">Доход</div>
-            <div class="text-lg font-bold">56 000 ₽</div>
+            <div class="text-lg font-bold">{{ stats?.price }}</div>
           </div>
           <div class="flex aspect-square h-10 items-center justify-center rounded-lg bg-green-100">
             <i class="pi pi-fw pi-money-bill" />
           </div>
         </div>
         <div class="text-sm">
-          <span class="font-medium text-green-500">+25%</span> с прошлой недели
+          <span class="font-medium text-green-500">{{ stats?.last_price }}</span>
+          последний заказ
         </div>
       </div>
 
@@ -45,14 +84,15 @@ function getGradient(ctx: any, chartArea: any) {
         <div class="mb-4 flex items-start justify-between text-black">
           <div>
             <div class="mb-2 font-medium">Клиенты</div>
-            <div class="text-lg font-bold">24314</div>
+            <div class="text-lg font-bold">{{ stats?.clients_count }}</div>
           </div>
           <div class="flex aspect-square h-10 items-center justify-center rounded-lg bg-red-100">
             <i class="pi pi-fw pi-user" />
           </div>
         </div>
         <div class="text-sm">
-          <span class="font-medium text-green-500">520</span> новых клиентов
+          <span class="font-medium text-green-500">{{ stats?.new_clients }}</span>
+          новых клиентов
         </div>
       </div>
 
@@ -60,80 +100,104 @@ function getGradient(ctx: any, chartArea: any) {
         <div class="mb-4 flex items-start justify-between text-black">
           <div>
             <div class="mb-2 font-medium">Отзывы</div>
-            <div class="text-lg font-bold">143 новых</div>
+            <div class="text-lg font-bold">{{ stats?.new_reviews }} новых</div>
           </div>
           <div class="flex aspect-square h-10 items-center justify-center rounded-lg bg-purple-100">
             <i class="pi pi-fw pi-comments" />
           </div>
         </div>
-        <div class="text-sm"><span class="font-medium text-green-500">46</span> отвеченных</div>
       </div>
     </div>
 
-    <div class="mb-8 flex gap-4">
-      <Chart
-        class="w-full"
-        type="line"
-        :options="{
-          aspectRatio: 3 / 1
-        }"
-        :data="{
-          labels: [
-            '01.08',
-            '02.08',
-            '03.08',
-            '04.08',
-            '05.08',
-            '06.08',
-            '07.08',
-            '08.08',
-            '09.08',
-            '10.08',
-            '11.08',
-            '12.08',
-            '13.08',
-            '14.08',
-            '15.08',
-            '16.08',
-            '17.08',
-            '18.08',
-            '19.08',
-            '20.08',
-            '21.08',
-            '22.08',
-            '23.08',
-            '24.08',
-            '25.08',
-            '26.08',
-            '27.08',
-            '28.08',
-            '29.08',
-            '30.08',
-            '31.08'
-          ],
-          datasets: [
-            {
-              label: 'Количество заказов',
-              borderColor: 'rgba(0, 147, 233, 1)',
-              yAxisID: 'y',
-              tension: 0,
-              data: [
-                67, 81, 86, 95, 96, 99, 82, 78, 84, 78, 87, 88, 102, 103, 77, 81, 99, 103, 100, 88,
-                96, 69, 77, 102, 86, 76, 96, 84, 94, 78, 102
-              ],
-              fill: true,
-              backgroundColor: function (context: any) {
-                const chart = context.chart
-                const { ctx, chartArea } = chart
+    <div class="flex gap-8">
+      <div class="flex-1">
+        <h2 class="mb-2 text-center text-lg font-bold">Клиенты</h2>
+        <Chart
+          class="mb-8 w-full"
+          type="line"
+          :options="{
+            aspectRatio: 3 / 1
+          }"
+          :data="{
+            labels: labels,
+            datasets: [
+              {
+                label: 'Количество заказов',
+                borderColor: 'rgba(255, 179, 71, 1)',
+                yAxisID: 'y',
+                tension: 0,
+                data: ordersCount,
+                fill: true,
+                backgroundColor: function (context: any) {
+                  const chart = context.chart
+                  const { ctx, chartArea } = chart
 
-                // This case happens on initial chart load
-                if (!chartArea) return
-                return getGradient(ctx, chartArea)
+                  // This case happens on initial chart load
+                  if (!chartArea) return
+                  let gradient = ctx.createLinearGradient(0, chartArea.bottom, 0, chartArea.top)
+                  gradient.addColorStop(1, 'rgba(255, 179, 71, 0.5)')
+                  gradient.addColorStop(0, 'rgba(255, 179, 71, 0)')
+                  return gradient
+                }
+              },
+              {
+                label: 'Новых пользователей',
+                borderColor: 'rgba(127, 0, 255, 1)',
+                yAxisID: 'y',
+                tension: 0,
+                data: newUsersCount,
+                fill: true,
+                backgroundColor: function (context: any) {
+                  const chart = context.chart
+                  const { ctx, chartArea } = chart
+
+                  // This case happens on initial chart load
+                  if (!chartArea) return
+                  let gradient = ctx.createLinearGradient(0, chartArea.bottom, 0, chartArea.top)
+                  gradient.addColorStop(1, 'rgba(127, 0, 255, 0.5)')
+                  gradient.addColorStop(0, 'rgba(127, 0, 255, 0)')
+                  return gradient
+                }
               }
-            }
-          ]
-        }"
-      />
+            ]
+          }"
+        />
+      </div>
+
+      <div class="flex-1">
+        <h2 class="mb-2 text-center text-lg font-bold">Доходы</h2>
+        <Chart
+          class="w-full"
+          type="line"
+          :options="{
+            aspectRatio: 3 / 1
+          }"
+          :data="{
+            labels: labels,
+            datasets: [
+              {
+                label: 'Доход',
+                borderColor: 'rgba(255, 75, 43, 1)',
+                yAxisID: 'y',
+                tension: 0,
+                data: income,
+                fill: true,
+                backgroundColor: function (context: any) {
+                  const chart = context.chart
+                  const { ctx, chartArea } = chart
+
+                  // This case happens on initial chart load
+                  if (!chartArea) return
+                  let gradient = ctx.createLinearGradient(0, chartArea.bottom, 0, chartArea.top)
+                  gradient.addColorStop(1, 'rgba(255, 75, 43, 0.5)')
+                  gradient.addColorStop(0, 'rgba(255, 75, 43, 0)')
+                  return gradient
+                }
+              }
+            ]
+          }"
+        />
+      </div>
     </div>
   </main>
 </template>
