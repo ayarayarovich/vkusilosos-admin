9f74b007bda0a19789f874e0c22c47aefc4650ad
Ayyar Khalilov <super.ayar-04@yandex.ru>
2024-01-27 17:40:26 +0300

    fix null checks in user details

--- src/features/users/UserDetails.vue
+++ src/features/users/UserDetails.vue
@@ -3,57 +3,63 @@
         <div class="flex items-end leading-none">
             <div>ID</div>
             <div class="mx-2 grow border-b-2 border-dotted border-black border-opacity-50"></div>
-            <div>{{ user.id || 'Нет данных' }}</div>
+            <div>
+                <template v-if="user.id != null">{{ user.id }}</template>
+                <template v-else>Нет данных</template>
+            </div>
         </div>
 
         <div class="flex items-end leading-none">
             <div>Имя</div>
             <div class="mx-2 grow border-b-2 border-dotted border-black border-opacity-50"></div>
-            <div>{{ user.name || 'Нет данных' }}</div>
+            <div>
+                <template v-if="user.name != null">{{ user.name }}</template>
+                <template v-else>Нет данных</template>
+            </div>
         </div>
 
         <div class="flex items-end leading-none">
             <div>Фамилия</div>
             <div class="mx-2 grow border-b-2 border-dotted border-black border-opacity-50"></div>
-            <div>{{ user.surname || 'Нет данных' }}</div>
+            <div>
+                <template v-if="user.surname != null">{{ user.surname }}</template>
+                <template v-else>Нет данных</template>
+            </div>
         </div>
 
         <div class="flex items-end leading-none">
             <div>Телефон</div>
             <div class="mx-2 grow border-b-2 border-dotted border-black border-opacity-50"></div>
-            <div>{{ user.phone || 'Нет данных' }}</div>
+            <div>
+                <template v-if="user.phone != null">{{ user.phone }}</template>
+                <template v-else>Нет данных</template>
+            </div>
         </div>
 
         <div class="flex items-end leading-none">
             <div>Бонусы</div>
             <div class="mx-2 grow border-b-2 border-dotted border-black border-opacity-50"></div>
-            <div>{{ user.bonuses || 'Нет данных' }}</div>
+            <div>
+                <template v-if="user.bonuses != null">{{ user.bonuses }}</template>
+                <template v-else>Нет данных</template>
+            </div>
         </div>
 
         <div class="flex items-end leading-none">
             <div>День рождения</div>
             <div class="mx-2 grow border-b-2 border-dotted border-black border-opacity-50"></div>
-            <div>{{ user.birthday ? dateFormat(user.birthday, 'dd.mm.yyyy') : 'Нет данных' }}</div>
+            <div>
+                <template v-if="user.birthday != null">{{ user.birthday }}</template>
+                <template v-else>Нет данных</template>
+            </div>
         </div>
 
         <div class="flex items-end leading-none">
             <div>Роль</div>
             <div class="mx-2 grow border-b-2 border-dotted border-black border-opacity-50"></div>
             <div>
-                <template v-if="user.role">
-                    <Tag
-                        v-if="user.role === 'user'"
-                        icon="pi pi-user"
-                        value="Обычный"
-                        severity="info"
-                    ></Tag>
-                    <Tag
-                        v-else-if="user.role === 'admin'"
-                        icon="pi pi-bolt"
-                        value="Админ"
-                        severity="warning"
-                    ></Tag>
-                    <Tag v-else :value="user.role" severity="info" />
+                <template v-if="user.role != null">
+                    <UserRoleBadge :code="user.role" />
                 </template>
                 <span v-else>Нет данных</span>
             </div>
@@ -63,26 +69,8 @@
             <div>Статус</div>
             <div class="mx-2 grow border-b-2 border-dotted border-black border-opacity-50"></div>
             <div>
-                <template v-if="user.status">
-                    <Tag
-                        v-if="user.status === 0"
-                        icon="pi pi-lock"
-                        value="Заблокирован"
-                        severity="danger"
-                    />
-                    <Tag
-                        v-else-if="user.status === 1"
-                        icon="pi pi-check-circle"
-                        value="Активен"
-                        severity="success"
-                    />
-                    <Tag
-                        v-else-if="user.status === 2"
-                        icon="pi pi-clock"
-                        value="На модерации"
-                        severity="warning"
-                    />
-                    <Tag v-else :value="user.status" severity="info" />
+                <template v-if="user.status != null">
+                    <UserStatusBadge :code="user.status" />
                 </template>
                 <span v-else>Нет данных</span>
             </div>
@@ -94,6 +82,8 @@
 import type { IUser } from './interfaces'
 import { inject } from 'vue'
 import dateFormat from 'dateformat'
+import UserRoleBadge from './UserRoleBadge.vue'
+import UserStatusBadge from './UserStatusBadge.vue'
 
 const dialogRef = inject('dialogRef') as any
 const user = dialogRef.value.data.user as IUser
